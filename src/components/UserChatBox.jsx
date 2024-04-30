b69070c515e601ead1895cc55b322cc5f1fb84c4
hafiznurrahman <hafiznurrahman.dev@gmail.com>
2024-05-01 06:21:44 +0700

    update homepage and user chat box

--- src/components/UserChatBox.jsx
+++ src/components/UserChatBox.jsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
 import Avatar from "./Avatar.jsx";
 
 export default function UserChatBox({ image, alt, name }) {
+  const nameClean = name.replace(/\s/g, "")
+  
+  
   return (
-    <button className="w-full h-auto flex items-center gap-3 p-2">
+    <Link
+      to={`/chat/${nameClean}`}
+      className="w-full h-auto flex items-center gap-3 p-2"
+    >
       <div className="w-12 h-12">
         <Avatar image={alt} alt={alt} />
       </div>
       <span className="font-bold truncate">{name || "Name"}</span>
-    </button>
+    </Link>
   );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,16 +5,7 @@ export default function Homepage() {
     <>
       <section className="flex flex-col gap-3">
         <UserChatBox image="" alt="" name="Your Username" />
-        <UserChatBox image="" alt="" name="Your Username" />
-        <UserChatBox image="" alt="" name="Your Username" />
-        <UserChatBox image="" alt="" name="Your Username" />
-        <UserChatBox image="" alt="" name="Your Username" />
-        <UserChatBox image="" alt="" name="Your Username" />
-        <UserChatBox image="" alt="" name="Your Username" />
-        <UserChatBox image="" alt="" name="Your Username" />
-        <UserChatBox image="" alt="" name="Your Username" />
-        <UserChatBox image="" alt="" name="Your Username" />
-
+       
       </section>
     </>
   );
