b9c1fc55f9815c6ac8222a4871778e4a0a0a1178
hafiznurrahman <hafiznurrahman.dev@gmail.com>
2024-05-01 09:57:58 +0700

    update homepage

--- src/components/UserProfileInformation.jsx
+++ src/components/UserProfileInformation.jsx
@@ -2,6 +2,8 @@ import Avatar from "./Avatar.jsx";
 import { Link } from "react-router-dom";
 
 export default function UserProfileInformation({ image, alt, name }) {
+  const defaultName = name || "username";
+  const nameClean = defaultName.toLowerCase().replace(/\s/g, "");
   return (
     <section className="w-full h-auto flex items-center gap-3 bg-dark2 border-b border-dark1 p-2">
       <Link to="../" className="font-bold text-xs">
@@ -10,8 +12,8 @@ export default function UserProfileInformation({ image, alt, name }) {
       <div className="w-10 h-10">
         <Avatar image={image} alt={alt} />
       </div>
-      <Link to="/" className="w-1/3 truncate">
-        {name || "Name"}
+      <Link to={`/profile/${nameClean}`} className="w-1/3 truncate">
+        {defaultName}
       </Link>
     </section>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import UserChatBox from "../components/UserChatBox.jsx";
+import SearchBox from "../components/SearchBox.jsx";
 
 export default function HomePage() {
   return (
     <>
+      <SearchBox />
       <section className="flex flex-col gap-3">
         <UserChatBox image="" alt="" name="Your Username" />
+        <UserChatBox image="" alt="" name="Fanny daratt" />
+        <UserChatBox image="" alt="" name="Moskov gamtengg" />
       </section>
     </>
   );
